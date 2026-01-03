7ca39554452bca93b8e1ddff086917dd8932e50a
Lucas Carlson <lucas@carlson.net>
2026-01-03 08:56:58 -0800

    Fix notes formula SHA256

--- Formula/notes.rb
+++ Formula/notes.rb
@@ -2,7 +2,7 @@ class Notes < Formula
   desc "Fuzzy search and browse Apple Notes from CLI"
   homepage "https://github.com/cardmagic/notes"
   url "https://registry.npmjs.org/@cardmagic/notes/-/notes-1.1.0.tgz"
-  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
+  sha256 "7b949204bbc07587dfdad7587b784a6547c403a72da4d88c5d5192ff1e489c6c"
   license "MIT"
 
   depends_on "node"
